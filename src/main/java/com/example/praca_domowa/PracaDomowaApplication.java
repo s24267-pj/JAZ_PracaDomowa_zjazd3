3796f119f408fdbd55ad2812b3dfebfa7a3ec600
Damian Ligeza <s24267@pjwstk.edu.pl>
2023-04-24 20:59:40 +0200

    Initial commit.

---
+++ src/main/java/com/example/praca_domowa/PracaDomowaApplication.java
@@ -0,0 +1,13 @@
+package com.example.praca_domowa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracaDomowaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracaDomowaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/praca_domowa/PracaDomowaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.praca_domowa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracaDomowaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
